f27192d090d34d7ddbe14bc7b9440929baa90186
ratidzo <ratygore@gmail.com>
2024-01-08 10:22:25 +0200

    setup: finish project setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
-import './styles/globals.css'
 import { cn } from '@/lib/utils'
-
+import { ThemeProvider } from '@/components/theme-provider'
+import './styles/globals.css'
 
 export const fontsans = FontSans({ 
   subsets: ['latin'],
@@ -25,7 +25,14 @@ export default function RootLayout({
         "min-h-screen bg-background font-sans antialiased",
         fontsans.variable
       )}>
-        {children}
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from '@/components/ui/button'
-
 
 export default function Home() {
+
   return (
     <div>
-      <Button className='bg-primary text-primary-foreground'>Click me</Button>
+        Welcome to Wire File
     </div>
   )
 }
