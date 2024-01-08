ec445c1ddcdeac39a5abe445a3ead8f6af10d9a7
ratidzo <ratygore@gmail.com>
2024-01-08 13:43:52 +0200

    dev: setup nav links and buttons

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@
 export default function Home() {
 
   return (
-    <div>
-        Welcome to Wire File
-    </div>
+    <>
+    </>
   )
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,27 @@
+import { Button } from "@/components/ui/button"
+
 
 export default function Navbar() {
 
     return (
-        <nav>
-            nav bar
+        <nav className="flex justify-between items-center py-6 px-10">
+            <h1 className="text-2xl font-semibold text-indigo-500">
+                Wired File
+            </h1>
+            <div>
+                <Button variant="link">
+                    Product
+                </Button>
+                <Button variant="link">
+                    Company
+                </Button>
+                <Button variant="link">
+                    FAQ
+                </Button>
+            </div>
+            <Button>
+                Sign in
+            </Button>
         </nav>
     )
 }
