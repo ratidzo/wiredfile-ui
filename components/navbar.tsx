8f86c3af2931a0ff2b8f81374cb5e08730dbd8df
ratidzo <ratygore@gmail.com>
2024-01-10 20:21:02 +0200

    dev: setup theme control dropdown menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -66,7 +66,7 @@ export default function Navbar() {
             {/* responsive menu */}
             <div className="relative">
                 {/* nav links */}
-                <ul className="w-full absolute left-0 pl-4">
+                <ul className="w-full absolute left-0">
                     {
                         links.map(link => (
                             <li key={link.name} className="my-2">
@@ -79,12 +79,35 @@ export default function Navbar() {
                         ))
                     }
                     {/* theme control dropdown menu */}
-                    <li>
-                        <Button className="ml-4">
-                            Theme
-                        </Button>
+                    <li className="mt-4">
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="outline" size="icon" className="ml-3">
+                                    <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                                    <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                                    <span className="sr-only">Toggle theme</span>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="start">
+                                <DropdownMenuLabel>
+                                    Theme
+                                </DropdownMenuLabel>
+                                <DropdownMenuItem className="gap-2 w-[13.375rem]" onClick={() => setTheme("light")}>
+                                    <SunIcon className="h-[1.2rem] w-[1.2rem]"/>
+                                    <h1>Light</h1>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem className="gap-2 w-[13.375rem]" onClick={() => setTheme("dark")}>
+                                    <MoonIcon className="h-[1.2rem] w-[1.2rem]"/>
+                                    <h1>Dark</h1>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem className="gap-2 w-[13.375rem]" onClick={() => setTheme("system")}>
+                                    <GearIcon className="h-[1.2rem] w-[1.2rem]"/>
+                                    <h1>System</h1>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                     </li>
-                    <Separator className="my-4" />
+                    <Separator className="my-6" />
                     <Button className="w-full">
                         Sign in
                     </Button>
