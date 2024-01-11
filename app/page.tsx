91197053227f3427cf8286fa820ec750738b468a
ratidzo <ratygore@gmail.com>
2024-01-11 18:41:47 +0200

    dev: add landing page messages and styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,16 @@ export default function Home() {
 
   return (
     <div className="flex justify-center">
-      <h1 className="text-4xl text-indigo-300">
-        Landing Page
-      </h1>
+      <section className="flex flex-col items-center w-full pt-[5.44rem]">
+        <div>
+          <h1 className="text-5xl font-extrabold py-2">Keep your school's data</h1>
+          <h1 className="text-5xl font-extrabold text-indigo-500 py-2">Wired to the Cloud.</h1>
+        </div>
+        <p className="mt-[1.31rem] text-2xl w-[48vw]">
+            Manage all data from one place. <span className="text-indigo-500 font-bold">Wired File</span> makes it easy
+            to <span className="font-bold" >store</span>, <span className="font-bold">secure</span> and <span className="font-bold">retrieve</span> records.
+        </p> 
+      </section>
     </div>
   )
 }
