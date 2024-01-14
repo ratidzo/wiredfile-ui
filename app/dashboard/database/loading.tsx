36475b0d9c0d7c7ef96a6356097db2e5ef540219
ratidzo <ratygore@gmail.com>
2024-01-15 01:57:11 +0200

    dev: set loading animation - install material-ui

---
+++ app/dashboard/database/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+
+    return (
+        <div>
+            <h1 className="animate-pulse">
+                Loading ...
+            </h1>
+        </div>
+    )
+}
--- app/dashboard/database/page.tsx
+++ app/dashboard/database/page.tsx
@@ -1,22 +1,27 @@
 "use client"
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { columns } from "./students/columns";
 import { StudentsTable } from "./students/students-table";
 import { Button } from "@/components/ui/button";
 import useSWR from "swr";
+import LinearProgress from '@mui/material/LinearProgress'
+import Box from '@mui/material/Box'
 
 import {
     Input
 } from '@/components/ui/input'
-
+import {
+    Skeleton
+} from '@/components/ui/skeleton'
 import {
     MagnifyingGlassIcon, PlusIcon
 } from '@radix-ui/react-icons'
+import { CircularProgress } from "@mui/material";
 
 export default function Database() {
    const [students, setStudents] = useState()
    const [loading, setLoading] = useState(true)
-   const [search, setSearch] = useState('')
+//    const [search, setSearch] = useState('')
    const [filter, setFilter]  = useState('')
    const [page, setPage] = useState(1)
    
@@ -40,26 +45,29 @@ export default function Database() {
    }
 
    const {data, error, isLoading} = useSWR(API_URL, fetcher)
-   if (isLoading){
-    return <div className="flex w-[80vw] min-h-[40vw] justify-center items-center">
-        <h1 className="animate-pulse">
-            Loading ...
-        </h1>
-    </div>
-   }
+   
     return (
         <div className="py-10 flex flex-col w-[75vw]">
             <div className="flex justify-between py-4">
                 <div className="flex gap-2">
                     <Input
-                    value={search}
-                    onChange={(event) => setSearch(event.target.value)}
+                    value={filter}
+                    // onKeyDown={(event)=> {
+                    //     if(event.key==='Enter') {
+                    //         setFilter(search)
+                    //     }
+                    // }}
+                    onChange={(event) => {
+                        setPage(1)
+                        setFilter(event.target.value)}}
                     type="text"
                     placeholder="Search students here ..."
 
                     className="w-[25vw]"  />
 
-                    <Button variant="ghost" onClick={() => setFilter(search)}>
+                    <Button variant="ghost" 
+                    // onClick={() => setFilter(filter)}
+                    >
                         <MagnifyingGlassIcon className="h-[1.2rem] w-[1.2rem]" />
                     </Button>
                     <Button variant="default" className="gap-2 ml-8">
@@ -68,28 +76,45 @@ export default function Database() {
                     </Button>
                 </div>
                 <h1 className=" text-2xl">
-                        All students
+                        All Students
                 </h1>
                 
                     
             </div>
-            { data && <StudentsTable columns={columns} data={data.results} />}
-            { data && 
-              (<div className="flex gap-4 mt-2">
-              <Button
-               disabled={page === 1}
-               variant={"ghost"} onClick={() => {setPage(page - 1)}}>
-                  Previous Page
-              </Button>
-              <Button
-               disabled={page === Math.ceil(data.count / 10)}
-               variant={"ghost"} onClick={() => {setPage(page + 1)}}>
-                  Next Page
-              </Button>
-              <Button variant={"ghost"}>
-                Page {page} of {Math.ceil(data.count / 10)}
-              </Button>
-          </div>)
+           
+            {
+                isLoading ? (
+                    
+                    <div className="flex justify-center h-[40vh] items-center w-[75vw] py-6">
+                       <Box sx={{ display: 'flex'}}>
+                        <CircularProgress color="inherit"/>
+                       </Box>
+                    </div>
+                        
+                    
+                ): 
+
+                (
+                       
+                <>
+                    <StudentsTable columns={columns} data={data.results} />
+                    <div className="flex gap-4 mt-2">
+                        <Button
+                        disabled={page === 1}
+                        variant={"ghost"} onClick={() => {setPage(page - 1)}}>
+                            Previous Page
+                        </Button>
+                        <Button
+                        disabled={page === Math.ceil(data.count / 10)}
+                        variant={"ghost"} onClick={() => {setPage(page + 1)}}>
+                            Next Page
+                        </Button>
+                        <Button variant={"ghost"}>
+                            Page {page} of {Math.ceil(data.count / 10)}
+                        </Button>
+                    </div>
+                </>
+                )
             }
        </div>
     )
